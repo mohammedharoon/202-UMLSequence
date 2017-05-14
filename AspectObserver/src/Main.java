7f7021f5e75191cfa5a77f55215d40a74de60e0b
Mohammed Haroon Shareef <mohammedharoon.shareef@sjsu.edu>
2017-05-13 17:16:29 -0700

    minor changes

--- AspectObserver/src/Main.java
+++ AspectObserver/src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main
 {
-    public static void main(String [] args)
+    public static void main(String [] args) throws Exception
     {
         TheEconomy s = new TheEconomy();
         Pessimist p = new Pessimist(s);
